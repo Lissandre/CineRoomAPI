40fba7388c8628d0f55636d9a24885928c1dc8d7
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-07 18:31:24 +0100

    env

--- config/index.js
+++ config/index.js
@@ -1,8 +1,23 @@
-const config = {
-  db: {
-    url: 'localhost:27017',
-    name: 'CineRoom'
+import dotenv from 'dotenv'
+const myEnv = dotenv.config()
+
+let config
+
+if(myEnv.parsed.config === 'dev'){
+  config = {
+    db: {
+      url: 'mongodb://localhost:27017/CineRoom'
+    }
   }
 }
+else if(myEnv.parsed.config === 'prod') {
+  config = {
+    db: {
+      url: `mongodb+srv://${myEnv.parsed.dbUSER}:${myEnv.parsed.dbPASSWORD}@${myEnv.parsed.dbURL}/${myEnv.parsed.dbNAME}?retryWrites=true&w=majority`
+    }
+  }
+}
+
+console.log(process);
 
 export default config
